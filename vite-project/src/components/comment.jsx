f97d50d1d3f0f784ac09c021d95207b542317e16
Sameer Gurung <Sameer.Gurung@kpmg.co.uk>
2026-01-07 16:10:50 +0000

    added a delete button that conditionally renders based on username

--- vite-project/src/components/comment.jsx
+++ vite-project/src/components/comment.jsx
@@ -1,4 +1,35 @@
+import axios from "axios";
+
 function Comment({ comment }) {
+  let isAuthor;
+  if (comment.author === "grumpy19") {
+    isAuthor = true;
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios
+      .delete(
+        `https://nc-news-xnco.onrender.com/api/comments/${comment.comment_id}`
+      )
+      .then((response) => {
+        console.log(response);
+      });
+    alert("comment has been deleted");
+  };
+  if (isAuthor) {
+    return (
+      <div>
+        <li>
+          <p>{comment.author}</p>
+          <p>{comment.created_at.slice(0, 10)}</p>
+          <p>{comment.body}</p>
+          <p>{comment.votes}</p>
+          <button onClick={handleSubmit}>DELETE</button>
+        </li>
+      </div>
+    );
+  }
   return (
     <div>
       <li>
@@ -10,4 +41,5 @@ function Comment({ comment }) {
     </div>
   );
 }
+
 export default Comment;
--- vite-project/src/components/commentform.jsx
+++ vite-project/src/components/commentform.jsx
@@ -1,14 +1,45 @@
+import axios from "axios";
 import { useState } from "react";
-
-useState;
+import { useParams } from "react-router-dom";
+useParams;
 function CommentForm() {
   const [comment, setComment] = useState("");
+  let params = useParams();
+  let article_id = parseInt(params.article_id);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (comment.length === 0) {
+      alert("comment must be filled out!");
+      return;
+    }
+    console.log(typeof comment);
+    console.log(typeof article_id);
+    axios
+      .post(
+        `https://nc-news-xnco.onrender.com/api/articles/${article_id}/comments`,
+        {
+          article_id: article_id,
+          body: comment,
+          author: "grumpy19",
+        }
+      )
+      .then((response) => {
+        console.log(response);
+      });
+    setComment("");
+    alert("comment has been added");
+  };
   return (
     <div class="commentForm">
-      <form action="submit">
-        {" "}
-        <input type="text" name="comment" id="" />
-        <button id="commentButton">Post</button>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={comment}
+          onChange={(event) => setComment(event.target.value)}
+        />
+        <button id="commentButton" type="submit">
+          Post
+        </button>
       </form>
     </div>
   );
